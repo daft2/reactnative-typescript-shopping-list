9c4cbec35b41da57b6c6738153204304d5ff8110
Daft <flushid@gmail.com>
2020-12-29 21:06:54 +0700

    initialized

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = ({title}: any) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkblue',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 23,
+    textAlign: 'center',
+  },
+});
+export default Header;
